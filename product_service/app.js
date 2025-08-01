93118f78577f663303fda74e387df8c9364b1f43
Srzya666 <gedebagussurya4@gmail.com>
2025-08-01 10:05:36 +0800

    add new project

---
+++ product_service/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const sequelize = require('./config/db');
+const db = require('./models');
+
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API aktif!'));
+
+// tambahkan routing kamu dibawah sini
+const productRoutes = require('./routes/product.route');
+app.use('/api/products', productRoutes);
+
+// Koneksi ke database
+sequelize.authenticate()
+  .then(() => {
+    console.log('✅ Terkoneksi ke MySQL');
+    return db.sequelize.sync({ alter: true });
+  })
+  .then(() => {
+    console.log('✅ Sinkronisasi selesai');
+  })
+  .catch((err) => {
+    console.error('❌ Gagal konek:', err);
+  });
+
+// Jalankan server
+app.listen(4002, () => {
+  console.log('🚀 Server jalan di http://localhost:4002');
+});
+
+
---
+++ transaction_service/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const sequelize = require('./config/db');
+const db = require('./models');
+
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API aktif!'));
+
+// tambahkan routing kamu dibawah sini
+const transactionRoutes = require('./routes/transaction.route');
+
+app.use('/api/transactions', transactionRoutes);
+
+// Koneksi ke database
+sequelize.authenticate()
+  .then(() => {
+    console.log('✅ Terkoneksi ke MySQL');
+    return db.sequelize.sync({ alter: true });
+  })
+  .then(() => {
+    console.log('✅ Sinkronisasi selesai');
+  })
+  .catch((err) => {
+    console.error('❌ Gagal konek:', err);
+  });
+
+// Jalankan server
+app.listen(3000, () => {
+  console.log('🚀 Server jalan di http://localhost:3000');
+});
+
+
